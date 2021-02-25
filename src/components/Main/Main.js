aebb8c109641e45a89adcfa3670fa653aed1d9bd
Sergei Kompanietc <delightvlg@gmail.com>
2021-02-25 12:26:17 +0300

    fix - Promo added

--- src/components/Main/Main.js
+++ src/components/Main/Main.js
@@ -1,17 +1,14 @@
 import React from 'react';
-import NavTab from '../NavTab/NavTab';
 import AboutProject from '../AboutProject/AboutProject';
 import Techs from '../Techs/Techs';
 import AboutMe from '../AboutMe/AboutMe';
+import Promo from '../Promo/Promo';
 import './Main.css';
 
 function Main() {
   return (
     <div className="main">
-      <section className="main__header">
-        <h1 className="main__title">Учебный проект студента факультета Веб-разработки.</h1>
-        <NavTab />
-      </section>
+      <Promo />
       <AboutProject />
       <Techs />
       <AboutMe />
